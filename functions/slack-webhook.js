b5fb246f42bb16b93aa067c96485e9809eb6058c
Adam Braimbridge <adam.braimbridge@ft.com>
2019-11-04 09:08:25 +0000

    authenticate

--- functions/slack-webhook.js
+++ functions/slack-webhook.js
@@ -1,38 +1,41 @@
-const axios = require('axios')
 const querystring = require('querystring')
+const axios = require('axios')
 
-const authenticate = () => {
+const authenticate = (httpMethod, token) => {
 	if (httpMethod !== 'POST') {
-		throw { statusCode: 405, body: 'Method Not Allowed' }
+		throw new Error({ statusCode: 405, body: 'Method Not Allowed' })
 	}
-	const { token } = querystring.parse(body)
 	const { SLACK_TOKEN } = process.env
 	if (!token || token !== SLACK_TOKEN) {
-		throw { statusCode: 401, body: 'Unauthorized' }
+		throw new Error({ statusCode: 401, body: 'Unauthorized' })
 	}
 	console.log('Authentication successful')
 }
 
-exports.handler = async ({ httpMethod, body }) => {
+exports.handler = async request => {
+	console.log({ request })
+	console.log(request)
+	const { httpMethod, body } = request
+	const { token } = querystring.parse(body)
 	try {
-		authenticate()
-		await axios.post(
-			response_url,
-			{
-				response_type: 'in_channel',
-				text,
-			},
-			{
-				headers: { 'content-type': 'application/json' },
-			}
-		)
-		return {
-			statusCode: 200,
-		}
+		authenticate(httpMethod, token)
+		// await axios.post(
+		// 	response_url,
+		// 	{
+		// 		response_type: 'in_channel',
+		// 		text,
+		// 	},
+		// 	{
+		// 		headers: { 'content-type': 'application/json' },
+		// 	}
+		// )
+		// return {
+		// 	statusCode: 200,
+		// }
 	} catch (error) {
 		console.error(error)
 		return {
-			// throw { statusCode: 405, body: 'Method Not Allowed' }
+			// throw new Error({ statusCode: 405, body: 'Method Not Allowed' })
 			// statusCode: 422, // Unprocessable Entity
 			// body: error.message,
 		}
