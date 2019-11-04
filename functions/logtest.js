dd98cfde1dafec7a0f8d3d899a483e12379b08ad
Adam Braimbridge <adam.braimbridge@ft.com>
2019-11-04 13:11:17 +0000

    Deleted unneeded function

--- functions/logtest.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-	console.log(event)
-	console.log(context)
-	return {
-		statusCode: 200,
-		body:
-			'<body style="text-align: center; padding: 10rem;"><h1>Is it 🍺⏰?</h1></body>',
-	}
-}
